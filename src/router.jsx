56272a20e1ee6a4a85afcb1f85ce6208ff233ddf
widi <116056487+wdiwbowo@users.noreply.github.com>
2024-10-16 15:17:05 +0700

    Update router.jsx

--- src/router.jsx
+++ src/router.jsx
@@ -8,24 +8,25 @@ import DeviceType from "./pages/DeviceType";
 import Company from "./pages/Company";
 import ProtectedRoute from "./components/ProtectedRoute";
 
-//Admin
+// Admin
 import DeviceAdmin from "./pages/DeviceAdmin";
 import Pengguna from "./pages/Pengguna";
 import LaporanPetugas from "./pages/LaporanPetugas";
 import ManajemanKonflik from "./pages/ManajemanKonflik";
 
 export const router = createBrowserRouter([
+    // Uncomment the line below if you want to protect the root route
     // { path: '/', element: <ProtectedRoute element={<App />} /> },
     { path: '/device', element: <ProtectedRoute element={<Device />} /> },
     { path: '/rules', element: <ProtectedRoute element={<Rules />} /> },
     { path: '/projects', element: <ProtectedRoute element={<Projects />} /> },
     { path: '/deviceType', element: <ProtectedRoute element={<DeviceType />} /> },
     { path: '/company', element: <ProtectedRoute element={<Company />} /> },
-    { path: '/login', element: </> },
+    { path: '/', element: <Login /> }, 
 
-//Admin
-{ path: '/deviceAdmin', element: <ProtectedRoute element={<DeviceAdmin />} /> },
-{ path: '/pengguna', element: <ProtectedRoute element={<Pengguna />} /> },
-{ path: '/laporanPetugas', element: <ProtectedRoute element={<LaporanPetugas />} /> },
-{ path: '/manajemanKonflik', element: <ProtectedRoute element={<ManajemanKonflik />} /> },
+    // Admin
+    { path: '/deviceAdmin', element: <ProtectedRoute element={<DeviceAdmin />} /> },
+    { path: '/pengguna', element: <ProtectedRoute element={<Pengguna />} /> },
+    { path: '/laporanPetugas', element: <ProtectedRoute element={<LaporanPetugas />} /> },
+    { path: '/manajemanKonflik', element: <ProtectedRoute element={<ManajemanKonflik />} /> },
 ]);
