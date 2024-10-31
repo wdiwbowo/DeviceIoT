7e1a3c4c1d4f361850bd516e31a7c5f3f0173140
widi <116056487+wdiwbowo@users.noreply.github.com>
2024-10-31 20:04:17 +0700

    Update Profile.jsx

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-import apiService from '../services/apiservice';
 import Swal from 'sweetalert2';
 import Navbar from '../components/Navbar';
 
