05b1191019c7e662144437daca59e68b49660cb7
widi <116056487+wdiwbowo@users.noreply.github.com>
2024-10-31 15:58:09 +0700

    Update Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,13 +1,11 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { jwtDecode } from 'jwt-decode'; // Corrected import statement
+import {jwtDecode} from 'jwt-decode'; // Corrected import statement
 import apiService from '../services/apiservice';
-import { FaEye, FaEyeSlash } from 'react-icons/fa'; // Importing icons
 
 const Login = () => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
-  const [showPassword, setShowPassword] = useState(false); // State for toggling password visibility
   const [error, setError] = useState(null);
   const navigate = useNavigate();
 
@@ -31,7 +29,7 @@ const Login = () => {
           navigate('/'); // Redirect to the Devices page for superadmins
         } else if (userRole === 'admin') {
           navigate('/'); // Redirect to the Admin page for admins
-        } else {
+         } else {
           setError('Peran pengguna tidak dikenal.');
         }
       } else {
@@ -40,7 +38,7 @@ const Login = () => {
     } catch (error) {
       setError('Error logging in: ' + error.message);
     }
-  };
+  };  
 
   return (
     <div className="flex items-center justify-center h-screen bg-gray-100">
@@ -61,26 +59,18 @@ const Login = () => {
               required
             />
           </div>
-          <div className="mb-6 relative"> {/* Added relative positioning */}
+          <div className="mb-6">
             <label htmlFor="password" className="block text-gray-700 mb-2">
               Password
             </label>
             <input
-              type={showPassword ? 'text' : 'password'} // Toggle input type
+              type="password"
               id="password"
               value={password}
               onChange={(e) => setPassword(e.target.value)}
-              className="w-full p-2 border border-gray-300 rounded pr-10" // Adjusted padding for icon
+              className="w-full p-2 border border-gray-300 rounded"
               required
             />
-            <button
-              type="button"
-              onClick={() => setShowPassword(!showPassword)} // Toggle password visibility
-              className="absolute right-2 top-2"
-              aria-label="Toggle password visibility"
-            >
-              {showPassword ? <FaEyeSlash className="text-gray-600" /> : <FaEye className="text-gray-600" />}
-            </button>
           </div>
           <button
             type="submit"
