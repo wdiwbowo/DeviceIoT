f4911c80c650a62dd78daed680137be1158a35cd
widi <116056487+wdiwbowo@users.noreply.github.com>
2024-10-31 15:18:15 +0700

    Update router.jsx

--- src/router.jsx
+++ src/router.jsx
@@ -7,6 +7,7 @@ import Projects from "./pages/Projects";
 import DeviceType from "./pages/DeviceType";
 import Company from "./pages/Company";
 import ProtectedRoute from "./components/ProtectedRoute";
+import Profile from "./pages/Profile";
 
 // Admin
 import DeviceAdmin from "./pages/DeviceAdmin";
@@ -15,18 +16,48 @@ import LaporanPetugas from "./pages/LaporanPetugas";
 import ManajemanKonflik from "./pages/ManajemanKonflik";
 
 export const router = createBrowserRouter([
-    // Uncomment the line below if you want to protect the root route
-    // { path: '/', element: <ProtectedRoute element={<App />} /> },
-    { path: '/device', element: <ProtectedRoute element={<Device />} /> },
-    { path: '/rules', element: <ProtectedRoute element={<Rules />} /> },
-    { path: '/projects', element: <ProtectedRoute element={<Projects />} /> },
-    { path: '/deviceType', element: <ProtectedRoute element={<DeviceType />} /> },
-    { path: '/company', element: <ProtectedRoute element={<Company />} /> },
     { path: '/', element: <Login /> },
 
+    {
+         path: '/device', 
+         element: <ProtectedRoute element={<Device />} /> 
+    },
+    { 
+        path: '/rules',
+        element: <ProtectedRoute element={<Rules />} /> 
+    },
+    { 
+        path: '/projects', 
+        element: <ProtectedRoute element={<Projects />} /> 
+    },
+    {
+        path: '/deviceType',
+        element: <ProtectedRoute element={<DeviceType />} /> 
+    },
+    {
+        path: '/company', 
+        element: <ProtectedRoute element={<Company />} /> 
+    },
+
     // Admin
-    { path: '/deviceAdmin', element: <ProtectedRoute element={<DeviceAdmin />} /> },
-    { path: '/pengguna', element: <ProtectedRoute element={<Pengguna />} /> },
-    { path: '/laporanPetugas', element: <ProtectedRoute element={<LaporanPetugas />} /> },
-    { path: '/manajemanKonflik', element: <ProtectedRoute element={<ManajemanKonflik />} /> },
+    { 
+        path: '/deviceAdmin', 
+        element: <ProtectedRoute element={<DeviceAdmin />} /> 
+    },
+    { 
+        path: '/pengguna', 
+        element: <ProtectedRoute element={<Pengguna />} /> 
+    },
+    { 
+        path: '/laporanPetugas', 
+        element: <ProtectedRoute element={<LaporanPetugas />} /> 
+    },
+    { 
+        path: '/manajemanKonflik', 
+        element: <ProtectedRoute element={<ManajemanKonflik />} />
+    },
+    {
+        path: '/profile',
+        element: <ProtectedRoute element={<Profile/>} />
+    }
 ]);
