2a585e35c99d80255a61ab23dab9638ee205da9f
widi <116056487+wdiwbowo@users.noreply.github.com>
2024-10-31 18:45:26 +0700

    Update Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import jwtDecode from 'jwt-decode';
+import {jwtDecode} from 'jwt-decode';
 import apiService from '../services/apiservice';
 
 const Login = () => {
