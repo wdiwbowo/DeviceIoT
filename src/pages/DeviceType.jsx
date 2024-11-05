f6e0c863cc5f7e67be0a9847d81ad798b58e883c
widi <116056487+wdiwbowo@users.noreply.github.com>
2024-11-05 15:20:14 +0700

    Update DeviceType.jsx

--- src/pages/DeviceType.jsx
+++ src/pages/DeviceType.jsx
@@ -182,7 +182,15 @@ const DeviceTypes = () => {
                                             {indexOfFirstItem + index + 1}
                                         </td>
                                         <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-700 dark:text-gray-300">{type.name || 'N/A'}</td>
-                                        <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-700 dark:text-gray-300">{type.active !== undefined ? (type.active ? "Yes" : "No") : 'N/A'}</td>
+                                        <td className="px-6 py-4 whitespace-nowrap text-sm">
+                    <span
+                        className={`inline-flex items-center px-2 py-1 rounded-full text-xs font-medium ${
+                            type.active ? 'bg-green-100 text-green-800' : 'bg-red-100 text-red-800'
+                        }`}
+                    >
+                        {type.active ? 'On' : 'Off'}
+                    </span>
+                </td>
                                         <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-700 dark:text-gray-300">{type.guid || 'N/A'}</td>
                                         <td className="px-6 py-4 whitespace-nowrap text-sm font-medium flex space-x-2">
                                             <button
