04c8a3ccff1fc92064d05fa4e8cd234d521ae9cb
widi <116056487+wdiwbowo@users.noreply.github.com>
2024-11-05 16:31:06 +0700

    Update Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import jwtDecode from 'jwt-decode'; // Corrected import for jwt-decode
+import {jwtDecode} from 'jwt-decode'; // Corrected import for jwt-decode
 import apiService from '../services/apiservice';
 import { FaEye, FaEyeSlash } from 'react-icons/fa';
 import Swal from 'sweetalert2';
